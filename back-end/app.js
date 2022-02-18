c7bcd26a2f9e4b99ebe5fc2f8f2fdfd9c658ab16
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2022-02-18 06:24:05 -0500

    created files

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 const cors = require("cors");
 const express = require("express");
+const teaController = require("./controllers/teasController");
 
 // CONFIGURATION
 const app = express();
@@ -8,29 +9,17 @@ const app = express();
 // MIDDLEWARE
 app.use(cors());
 app.use(express.json()); // Parse incoming JSON
-
+require("dotenv").config();
 // ROUTES
+app.use("/teas", teaController);
+
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.send("Welcome to Uncle Irohs Tea shop!");
 });
 
-/////////////////////////////////////
-// REMOVE AFTER SUCCESSFUL DEPLOYMENT
-/////////////////////////////////////
-const db = require("./db/dbConfig.js");
-
-app.get("/test", async (req, res) => {
-  try {
-    const allDays = await db.any("SELECT * FROM test");
-    res.json(allDays);
-  } catch (err) {
-    res.json(err);
-  }
-});
-
-/////////////////////////////////////
-// REMOVE AFTER SUCCESSFUL DEPLOYMENT
-/////////////////////////////////////
+app.get("*", (req, res) => {
+  res.status(404).send("Page you are looking for does not exist")
+})
 
 // EXPORT
 module.exports = app;
---
+++ back-end/controllers/teasController.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const teas = express.Router();
+const {} 
+const validateSpace = require("../validations/stringValidation.js");
+
+
+module.exports = teas;
